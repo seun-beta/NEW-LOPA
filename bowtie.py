c460aa084fea9fdbe4d5628612da655005ae4a81
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-06 12:05:56 +0100

    create bowtie program

---
+++ bowtie.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+
+top = Toplevel()
+
+
+
+
+
+
+
+
+def bowtie():
+    pass
+
+
+
+
+bow_tie = Button(root, text="Create Bow Tie Diagram", command=bowtie)
+bow_tie.grid(row=0, column=0, padx=10, pady=10)
+
+
+    
