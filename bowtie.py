3c01b92b579b02647afd0365f9ff857457df08a9
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-07 13:25:09 +0100

    add geometry

--- bowtie.py
+++ bowtie.py
@@ -5,18 +5,9 @@ import sqlite3
 
 root = Tk()
 
-
-
-
-
-
-
-
-
-
-root.geometry("1000x1000")
-square_canvas = Canvas(root, width=10000, height=10000, bg="white")
-square_canvas.grid(column=1, row=1, padx=10, pady=10)
+root.geometry("10000x10000")
+canvas = Canvas(root, width=10000, height=10000, bg="white")
+canvas.grid(padx=10, pady=10)
 
 
 # square_canvas.create_line(x1, y1, x2, y2, )
@@ -33,34 +24,42 @@ y2_line = 30
 
 conn = sqlite3.connect("lopa.db")
 cur = conn.cursor()
+
 cur.execute("""SELECT cause_id FROM Cause;
             """)
-data = cur.fetchall()
-print(data)
+cause_iter = cur.fetchall()
+for i in cause_iter:
 
-for i in data:
+    cur.execute("""SELECT cause_barrier_id FROM Cause_Barrier WHERE cause_id = ?;""", (i))
+    cause_barrier_iter = cur.fetchall()
+    print(cause_barrier_iter)
 
-    for i in range(5):
-    
-        square_canvas.create_line(x1_line, y1_line, x2_line, y2_line, fill="red")
+
+    for i in cause_barrier_iter:
+
+        canvas.create_line(x1_line, y1_line, x2_line, y2_line, fill="black", width=2)
         x1_line = x2_square
         x2_line = x1_line + 50
 
-
-
-        square_canvas.create_rectangle(x1_square, y1_square, x2_square, y2_square, fill="purple")
+        canvas.create_rectangle(x1_square, y1_square, x2_square, y2_square, fill="blue")
         x1_square = x1_line + 40
         x2_square = x1_square + 40
+    
+    x1_square = x1_square + 20 
+    y1_square = y1_square + 20
+    x2_square = x2_square + 20
+    y2_square = y2_square + 20
 
-    x1_square = 20 -20
-    y1_square = 40 -20
-    x2_square = 60 -20
-    y2_square = 20 -20
+    x1_line = x1_line + 20
+    y1_line = y1_line + 20
+    x2_line = x2_line + 20
+    y2_line = y2_line + 20
 
-    x1_line = 60 -20
-    y1_line = 30 -20
-    x2_line = 70 -20
-    y2_line = 30 -20
+
+
+
+
+
 
 
 
