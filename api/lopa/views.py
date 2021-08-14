57f31697da61b52f3a99546345a8cac7d3422bd5
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-14 11:21:34 +0100

    add response for model

--- api/lopa/views.py
+++ api/lopa/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+from .models import Cause
+from .serializers import CauseSerializer
+from lopa import serializers
+
+
+class Data(APIView):
+
+    def get(self, request):
+        cause = Cause.objects.all()
+        cause_data = CauseSerializer(cause)
+        return Response(cause_data.data)
