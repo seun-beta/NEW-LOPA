b9d8060387337860960a8eab2fd92dd272c35575
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-14 15:06:03 +0100

    remove extra models in serilizers

--- api/lopa/serializers.py
+++ api/lopa/serializers.py
@@ -5,50 +5,36 @@ from .models import Cause, CauseBarrier, Consequence, ConsequenceBarrier, Event
 
 
 class CauseSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Cause
-        model = CauseBarrier
-        model = Consequence
-        model = ConsequenceBarrier
-        model = Event
+
         fields = "__all__"
 
 
 class CauseBarrierSerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = Cause
         model = CauseBarrier
-        model = Consequence
-        model = ConsequenceBarrier
-        model = Event
         fields = "__all__"
 
 
 class ConsequenceSerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = Cause
-        model = CauseBarrier
         model = Consequence
-        model = ConsequenceBarrier
-        model = Event
         fields = "__all__"
 
 
 class ConsequenceBarrierSerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = Cause
-        model = CauseBarrier
-        model = Consequence
         model = ConsequenceBarrier
-        model = Event
         fields = "__all__"
 
 
 class EventSerializer(serializers.ModelSerializer):
+    
     class Meta:
-        model = Cause
-        model = CauseBarrier
-        model = Consequence
-        model = ConsequenceBarrier
         model = Event
         fields = "__all__"
