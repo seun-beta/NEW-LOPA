a6782b5c7ce32b1badc47df70db371bdcf55d18a
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-14 15:06:32 +0100

    add more endpoints

--- api/lopa/urls.py
+++ api/lopa/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Data.as_view()),
+    path('cause', views.CauseData.as_view()),
+    path('cause_barrier', views.CauseBarrierData.as_view()),
+    path('consequence', views.ConsequenceData.as_view()),
+    path('consequence_barrier', views.ConsequenceBarrierData.as_view()),
+    path('event', views.EventData.as_view()),
 ]
