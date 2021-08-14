3ffd32ac87d057ef33cf9b7fa8ef71719ce036e4
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-14 14:16:04 +0100

    add models to serializer

--- api/lopa/serializers.py
+++ api/lopa/serializers.py
@@ -1,9 +1,54 @@
 from django.db import models
 from rest_framework import serializers
 
-from .models import Cause
+from .models import Cause, CauseBarrier, Consequence, ConsequenceBarrier, Event
+
 
 class CauseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cause
+        model = CauseBarrier
+        model = Consequence
+        model = ConsequenceBarrier
+        model = Event
+        fields = "__all__"
+
+
+class CauseBarrierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cause
+        model = CauseBarrier
+        model = Consequence
+        model = ConsequenceBarrier
+        model = Event
+        fields = "__all__"
+
+
+class ConsequenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cause
+        model = CauseBarrier
+        model = Consequence
+        model = ConsequenceBarrier
+        model = Event
+        fields = "__all__"
+
+
+class ConsequenceBarrierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cause
+        model = CauseBarrier
+        model = Consequence
+        model = ConsequenceBarrier
+        model = Event
+        fields = "__all__"
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cause
+        model = CauseBarrier
+        model = Consequence
+        model = ConsequenceBarrier
+        model = Event
         fields = "__all__"
