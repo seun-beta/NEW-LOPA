766278e043576196b3e38c0d8ac7118b104e9146
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-14 11:20:52 +0100

    add models for legacy db

--- api/lopa/models.py
+++ api/lopa/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Cause(models.Model):
+    description = models.TextField(blank=True, null=True)       
+    initial_frequency = models.FloatField(blank=True, null=True)
+    event_id = models.IntegerField(blank=True, null=True)       
+    target_frequency = models.FloatField(blank=True, null=True) 
+
+    class Meta:
+        managed = False
+        db_table = 'cause'
+
+
+class CauseBarrier(models.Model):
+
+    description = models.TextField(blank=True, null=True)
+    pfd = models.FloatField(blank=True, null=True)
+    cause_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cause_barrier'
+
+
+class Consequence(models.Model):
+    description = models.TextField(blank=True, null=True)
+    initial_frequency = models.FloatField(blank=True, null=True)
+    target_frequency = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'consequence'
+
+
+class ConsequenceBarrier(models.Model):
+    description = models.TextField(blank=True, null=True)
+    pfd = models.FloatField(blank=True, null=True)
+    consequence_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'consequence_barrier'
+
+
+class Event(models.Model):
+    description = models.TextField(blank=True, null=True)
+    cause_id = models.IntegerField(blank=True, null=True)
+    consequence_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'event'
