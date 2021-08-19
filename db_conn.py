84d4303d88a27867827bc1517842e9b08fdc6ccb
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-19 13:54:46 +0100

    create temporary mysql connection command

---
+++ db_conn.py
@@ -0,0 +1,6 @@
+    conn = mysql.connector.connect(
+        host="lopasvr.mysql.database.azure.com",
+        user="lopasvr_user@lopasvr",
+        password="l0p@$vr_u$er",
+        database="lopaproject"
+    )
