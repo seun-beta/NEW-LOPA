97b52e4074471a45b14a89098cbdd0fbb0772a0e
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-08-20 06:20:24 +0100

    create db connection dump file

--- db_conn.py
+++ db_conn.py
@@ -1,6 +1,17 @@
+import mysql.connector
+
+def create_table():
     conn = mysql.connector.connect(
         host="lopasvr.mysql.database.azure.com",
         user="lopasvr_user@lopasvr",
         password="l0p@$vr_u$er",
-        database="lopaproject"
     )
+
+
+
+    cur = conn.cursor()
+    
+    cur.execute("CREATE DATABASE lopaproject")
+
+create_table()
+
